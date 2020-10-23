66c81e87793a559a2ba29f293a6ceae2ade7b5c0
Peter Yoachim <yoachim@uw.edu>
2020-10-22 18:56:44 -0700

    blargh

--- maf/generate_ss2.py
+++ maf/generate_ss2.py
@@ -18,11 +18,12 @@ with open('ss2_script.sh', 'w') as f:
             print('run_moving_calc.py --characterization inner --obsFile '+
                   '%s__%s_obs.txt ' % (run, pop) + 
                   '--opsimDb %s.db ' % run + '--orbitFile '+orbit_files[pop] + 
-                  ' --outDir %s_ss ' % run +
+                  ' --outDir %s_%s ' % (run, pop) +
                   '--opsimRun %s' % run,
                   file=f)
 
 with open('ss3_script.sh', 'w') as f:
     for run in runs:
-        print('run_moving_fractions.py --workDir %s_ss' % run,
-              file=f)
+        for pop in pops:
+            print('run_moving_fractions.py --workDir %s_%s' % (run, pop),
+                  file=f)
