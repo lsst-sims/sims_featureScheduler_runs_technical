6e8e3531e12a733e4ffc279bc89f418b7a04e79a
Peter Yoachim <yoachim@uw.edu>
2021-01-25 11:15:27 -0800

    more maf

--- pause_sim/maf.sh
+++ pause_sim/maf.sh
@@ -1,2 +1,6 @@
 python ../glance_dir.py --db pause_sim_v1.7_10yrs.db
 python ../scimaf_dir.py --db pause_sim_v1.7_10yrs.db
+python ../glance_dir.py --db pause2_pl183_v1.7_10yrs.db
+python ../scimaf_dir.py --db pause2_pl183_v1.7_10yrs.db
+python ../glance_dir.py --db pause2_pl91_v1.7_10yrs.db
+python ../scimaf_dir.py --db pause2_pl91_v1.7_10yrs.db
